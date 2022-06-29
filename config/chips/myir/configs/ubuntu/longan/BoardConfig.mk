4b2e25fef37add590ad10725b4f76db3b8150947
lichang70 <licy.li@myirtech.com>
2022-06-29 09:28:16 +0800

    FEAT: add config files

---
+++ config/chips/myir/configs/ubuntu/longan/BoardConfig.mk
@@ -0,0 +1,7 @@
+LICHEE_BRANDY_VER:=2.0
+LICHEE_BRANDY_DEFCONF:=sun50iw9p1_defconfig
+LICHEE_KERN_DEFCONF:=sun50iw9p1smp_longan_defconfig
+LICHEE_BUILDING_SYSTEM:=buildroot
+LICHEE_BR_VER:=201902
+LICHEE_BR_DEFCONF:=sun50iw9p1_longan_defconfig
+LICHEE_COMPILER_TAR=gcc-linaro-5.3.1-2016.05-x86_64_aarch64-linux-gnu.tar.xz
