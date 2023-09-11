1ec537b45ad1a5439d849f616ed0480fe0089ec6
Surpurple <2592716997@qq.com>
2023-09-11 14:36:34 +0800

    Initial commit

---
+++ config/chips/t113/configs/myir-image-yt113s3-emmc-core/longan/BoardConfig.mk
@@ -0,0 +1,9 @@
+LICHEE_KERN_DEFCONF:=myd_yt113s3_emmc_core_defconfig
+LICHEE_KERN_DEFCONF_RECOVERY:=sun8iw20p1smp_t113_recovery_defconfig
+LICHEE_BUILDING_SYSTEM:=buildroot
+LICHEE_BR_VER:=201902
+LICHEE_BR_DEFCONF:=myd_yt113s3_emmc_core_br_defconfig
+LICHEE_COMPILER_TAR=gcc-linaro-5.3.1-2016.05-x86_64_arm-linux-gnueabi.tar.xz
+LICHEE_COMPILER_HARD_TAR=gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf.tar.xz
+LICHEE_BRANDY_DEFCONF:=sun8iw20p1_auto_defconfig
+LICHEE_REDUNDANT_ENV_SIZE:=0x20000
