13ba3c722435d8814e076d4ac3a02e2d0ff19f6d
Surpurple <2592716997@qq.com>
2023-12-08 11:22:17 +0800

    FEAT: first commit

---
+++ config/chips/t113/configs/myir-image-yt113s3-emmc-core/bsp/BoardConfig.mk
@@ -0,0 +1,6 @@
+LICHEE_KERN_DEFCONF:=sun8iw20p1smp_t113_auto_defconfig
+LICHEE_BUILDING_SYSTEM:=buildroot
+LICHEE_BR_VER:=201902
+LICHEE_BR_DEFCONF:=sun8iw20p1_longan_defconfig
+LICHEE_COMPILER_TAR=gcc-linaro-5.3.1-2016.05-x86_64_arm-linux-gnueabi.tar.xz
+LICHEE_COMPILER_HARD_TAR=gcc-linaro-7.3.1-2018.05-x86_64_arm-linux-gnueabihf.tar.xz
